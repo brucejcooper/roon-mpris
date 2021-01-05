25d9a79671a42cbdf2aaf14a27df51021fe6d334
Bruce Cooper <bjc@amazon.com>
2021-01-05 15:54:02 +0800

    Small reliability fixes

--- index.js
+++ index.js
@@ -34,19 +34,22 @@ var zone;
 
 function zoneChanged(new_zone) {
     zone = new_zone;
-    console.log();
     var url = core.moo.transport.ws._url.substring(5);
-    mpris.metadata = {
-        'mpris:length': zone.now_playing.length * 1000 * 1000, // In microseconds
-        'mpris:artUrl': `http://${url}/image/${zone.now_playing.image_key}`,
-        'xesam:title': zone.now_playing.three_line.line1,
-        'xesam:album': zone.now_playing.three_line.line3,
-        'xesam:artist': zone.now_playing.three_line.line2.split(/\s+\/\s+/),
+    var now_playing = zone.now_playing;
+
+    if (now_playing) {
+        mpris.metadata = {
+            'mpris:length': now_playing.length * 1000 * 1000, // In microseconds
+            'mpris:artUrl': `http://${url}/image/${now_playing.image_key}`,
+            'xesam:title': now_playing.three_line.line1,
+            'xesam:album': now_playing.three_line.line3,
+            'xesam:artist': now_playing.three_line.line2.split(/\s+\/\s+/),
+        }    
     }
     mpris.playbackStatus = zone.state.charAt(0).toUpperCase() + zone.state.slice(1); 
     mpris.canGoNext = zone.is_next_allowed;
     mpris.canGoPrevious = zone.is_next_allowed;
-    // mpris.canPlay = zone.is_play_allowed; // the ubuntu dock disappears if it is set to false (while playing)
+    // mpris.canPlay = zone.is_play_allowed; // the ubuntu dock widget disappears if this is set to false (while playing)
     mpris.canPause = zone.is_pause_allowed;
     mpris.canSeek = zone.is_seek_allowed;
     console.log(zone);
@@ -70,16 +73,17 @@ const roon = new RoonApi({
     core_paired: function(core_) {
       core = core_;
 
-      
-   
+
       let transport = core.services.RoonApiTransport;
       transport.subscribe_zones(function(cmd, data) {
 
+        // When we connect, we receive a zones event.  When a zone chages, we receive a zones_changed.
+        // They are the same type, and we treat them the same.
         var zones = data.zones_changed || data.zones;
-
         if (zones) {
             for (var candidate of zones) {
-                if (candidate.display_name === mysettings.zone.name) {
+                // We only want to respond to our configured zone.
+                if (mysettings.zone && candidate.display_name === mysettings.zone.name) {
                     zoneChanged(candidate);
                 }
             }
@@ -162,8 +166,8 @@ if (argv.core) {
     console.log(`Connecting to Core at ws://${argv.core}:${argv.port}`)
     roon.ws_connect({ host: argv.core, port: argv.port});
 } else {
+    console.log("Autodiscovery of Core")
     roon.start_discovery();
-
 }
 
 
